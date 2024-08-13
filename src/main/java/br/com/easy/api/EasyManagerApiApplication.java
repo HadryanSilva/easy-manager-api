62560507068700efc6623d95ca650d146ec5b5fa
Hadryan Silva <hadryan2001@gmail.com>
2024-08-13 17:01:13 -0300

    Initial Commit

---
+++ src/main/java/br/com/easy/api/EasyManagerApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.easy.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyManagerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyManagerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.easy.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyManagerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
