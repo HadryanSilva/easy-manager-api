e8637562309afdca3616014fa2b2af0922d751c2
Hadryan Silva <hadryan2001@gmail.com>
2024-08-15 00:27:07 -0300

    Fix github build

--- src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
+++ src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
@@ -2,9 +2,10 @@ package br.com.easy.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EasyManagerApiApplicationTests {
 
 	@Test
