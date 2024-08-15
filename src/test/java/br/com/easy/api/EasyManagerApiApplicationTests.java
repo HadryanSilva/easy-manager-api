e800df2b246af3153f3e971a1bd562ae464ca0e1
Hadryan Silva <hadryan2001@gmail.com>
2024-08-15 03:26:40 -0300

    fix github actions build

--- src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
+++ src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
@@ -1,8 +1,6 @@
 package br.com.easy.api;
 
-import br.com.easy.api.config.SecurityConfig;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
