6f37ace24a8187a4ac2aa57930efb26a6d5a0e84
Hadryan Silva <hadryan2001@gmail.com>
2024-08-15 00:29:39 -0300

    Fix github build

--- src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
+++ src/test/java/br/com/easy/api/EasyManagerApiApplicationTests.java
@@ -1,9 +1,12 @@
 package br.com.easy.api;
 
+import br.com.easy.api.config.SecurityConfigTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
 
+@Import(SecurityConfigTest.class)
 @SpringBootTest
 @ActiveProfiles("test")
 class EasyManagerApiApplicationTests {
